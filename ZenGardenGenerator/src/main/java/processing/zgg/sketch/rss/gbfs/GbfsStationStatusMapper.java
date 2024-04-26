10af22b36ea9d9c27e0968735108928476d27f7c
gestorum <phil@phil-ThinkPad-P53>
2024-04-25 21:22:39 -0400

    Removes all auto-generated comments

--- ZenGardenGenerator/src/main/java/processing/zgg/sketch/rss/gbfs/GbfsStationStatusMapper.java
+++ ZenGardenGenerator/src/main/java/processing/zgg/sketch/rss/gbfs/GbfsStationStatusMapper.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package processing.zgg.sketch.rss.gbfs;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import processing.data.JSONObject;
 import processing.zgg.sketch.rss.gbfs.data.GbfsStationStatus;
 
 /**
